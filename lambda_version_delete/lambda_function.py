cfa12b2d793a341782fe9456484598fb61e06f05
s3lva-kumar <98340923+s3lva-kumar@users.noreply.github.com>
2023-07-24 16:16:00 +0530

    feat: add the lambda version delete function

---
+++ lambda_version_delete/lambda_function.py
@@ -0,0 +1,25 @@
+#!/bin/bash python3
+import boto3
+
+def lambda_handler(event, context): 
+    client = boto3.client('lambda', region_name="us-east-2")
+    response = client.list_functions(FunctionVersion='ALL')
+    version_count=[]
+    i=1
+    for x in response["Functions"]:
+        function_name=x["FunctionName"]
+        version = client.list_versions_by_function(FunctionName=function_name, MaxItems=100)
+        for y in version["Versions"]:
+            version_count.append(y["Version"])
+        print(f'{function_name}:count:{len(version_count)}:{version_count}')
+        while (len(version_count) > 10):
+            if (len(version_count) == 10):
+                print(f'{function}\'s version count first {len(version_count)}')
+                version_count.clear()
+                break
+            print(f'delete_version:-{function_name}:{version_count[i]}')
+            client.delete_function(FunctionName=function_name,Qualifier=version_count[i])
+            version_count.pop(i)
+        else:
+            print(f'{function_name}\'s version count is {len(version_count)}')
+            version_count.clear()
